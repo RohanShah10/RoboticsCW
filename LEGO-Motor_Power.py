6847276a2b8c42c82ad5b9f7500e2511ec0cf160
impRoboticsWeee <robotics@onenyb.info>
2025-01-24 17:08:13 +0000

    Add files via upload

--- LEGO-Motor_Power.py
+++ LEGO-Motor_Power.py
@@ -23,14 +23,14 @@ BP = brickpi3.BrickPi3() # Create an instance of the BrickPi3 class. BP will be 
 
 try:
     try:
-        BP.offset_motor_encoder(BP.PORT_A, BP.get_motor_encoder(BP.PORT_A)) # reset encoder B
+        BP.offset_motor_encoder(BP.PORT_B, BP.get_motor_encoder(BP.PORT_B)) # reset encoder B
     except IOError as error:
         print(error)
     
     while True:
         # The following BP.get_motor_encoder function returns the encoder value (what we want to use to control motor C's power).
         try:
-            power = BP.get_motor_encoder(BP.PORT_A) / 10
+            power = BP.get_motor_encoder(BP.PORT_B) / 10
             if power > 100:
                 power = 100
             elif power < -100:
@@ -38,10 +38,10 @@ try:
         except IOError as error:
             print(error)
             power = 0
-        BP.set_motor_power(BP.PORT_A, power)
-        print("  Motor A Status: ", BP.get_motor_status(BP.PORT_A))
+        BP.set_motor_power(BP.PORT_C, power)
         
         time.sleep(0.02)  # delay for 0.02 seconds (20ms) to reduce the Raspberry Pi CPU load.
+        print("Motor A target: %6d  Motor A position: %6d" % (power, BP.get_motor_encoder(BP.PORT_C)), "  Motor A Status: ", BP.get_motor_status(BP.PORT_C))
 
 except KeyboardInterrupt: # except the program gets interrupted by Ctrl+C on the keyboard.
     BP.reset_all()        # Unconfigure the sensors, disable the motors, and restore the LED to the control of the BrickPi3 firmware.
